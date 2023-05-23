6ad4fbf1078720df85090422ec6912801d3fb1ab
StM <smarcelli04@gmail.com>
2023-05-23 09:17:14 +0200

    tentativo di implementare una versione carina della richiesta di conferma

--- BlazorCrudDotNet7/Client/Services/ProductService/INoteService.cs
+++ BlazorCrudDotNet7/Client/Services/ProductService/INoteService.cs
@@ -5,10 +5,44 @@ namespace BlazorCrudDotNet7.Client.Services.NoteService
     public interface INoteService
     {
         List<Note> Notes { get; set; }
+        
+        /// <summary>
+        /// prende tutte le note dal DB
+        /// </summary>
+        /// <returns>
+        /// ritorna tutte le note all'interno di se stesso
+        /// </returns>
         Task GetNotes();
+
+        /// <summary>
+        /// prende una determinata nota dal DB
+        /// </summary>
+        /// <param name="id">id della nota da recuperare dal DB</param>
+        /// <returns>
+        /// ritorna la nota selezionata in caso di presenza nel DB, altrimenti null
+        /// </returns>
         Task<Note?> GetNoteById(int id);
+
+        /// <summary>
+        /// crea una nuova nota nel DB
+        /// </summary>
+        /// <param name="note">campo nota da implementare nel DB</param>
+        /// <returns></returns>
         Task CreateNote(Note note);
+
+        /// <summary>
+        /// aggiorna una nota specifica nel DB
+        /// </summary>
+        /// <param name="id">id della nota da modificare</param>
+        /// <param name="note">nota con tutti i campi da rimpiazzare</param>
+        /// <returns></returns>
         Task UpdateNote(int id, Note note);
+
+        /// <summary>
+        /// Elimina una determinata nota dal DB
+        /// </summary>
+        /// <param name="id">id della nota da eliminare</param>
+        /// <returns></returns>
         Task DeleteNote(int id);
     }
 }
--- BlazorCrudDotNet7/Client/Services/ProductService/NoteService.cs
+++ BlazorCrudDotNet7/Client/Services/ProductService/NoteService.cs
@@ -18,6 +18,11 @@ namespace BlazorCrudDotNet7.Client.Services.NoteService
 
         public List<Note> Notes { get; set; } = new List<Note>();
 
+        /// <summary>
+        /// crea una nuova nota nel DB
+        /// </summary>
+        /// <param name="note">campo nota da implementare nel DB</param>
+        /// <returns></returns>
         public async Task CreateNote(Note note)
         {
             await _http.PostAsJsonAsync("api/note", note);
@@ -25,12 +30,24 @@ namespace BlazorCrudDotNet7.Client.Services.NoteService
             _navigationManger.NavigateTo($"note");
         }
 
+        /// <summary>
+        /// Elimina una determinata nota dal DB
+        /// </summary>
+        /// <param name="id">id della nota da eliminare</param>
+        /// <returns></returns>
         public async Task DeleteNote(int id)
         {
             var result = await _http.DeleteAsync($"api/note/{id}");
             _navigationManger.NavigateTo("note");//notes
         }
 
+        /// <summary>
+        /// prende una determinata nota dal DB
+        /// </summary>
+        /// <param name="id">id della nota da recuperare dal DB</param>
+        /// <returns>
+        /// ritorna la nota selezionata in caso di presenza nel DB, altrimenti null
+        /// </returns>
         public async Task<Note?> GetNoteById(int id)
         {
             var result = await _http.GetAsync($"api/note/{id}");
@@ -41,6 +58,12 @@ namespace BlazorCrudDotNet7.Client.Services.NoteService
             return null;
         }
 
+        /// <summary>
+        /// prende tutte le note dal DB
+        /// </summary>
+        /// <returns>
+        /// ritorna tutte le note all'interno di se stesso
+        /// </returns>
         public async Task GetNotes()
         {
             var result = await _http.GetFromJsonAsync<List<Note>>("api/note");
@@ -48,6 +71,12 @@ namespace BlazorCrudDotNet7.Client.Services.NoteService
                 Notes = result;
         }
 
+        /// <summary>
+        /// aggiorna una nota specifica nel DB
+        /// </summary>
+        /// <param name="id">id della nota da modificare</param>
+        /// <param name="note">nota con tutti i campi da rimpiazzare</param>
+        /// <returns></returns>
         public async Task UpdateNote(int id, Note note)
         {
             await _http.PutAsJsonAsync($"api/note/{id}", note);
